3703ecc2f252fa807600118c5aa83abaaf8cc75c
ZloiY <NoRFolf1000@mail.ru>
2016-12-15 16:32:04 +0300

    seem's like multi thread

--- OS№4/Main.cpp
+++ OS№4/Main.cpp
@@ -11,109 +11,6 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 #define DEFAULT_PORT "5223"
-//#define DEFAULT_BUFLEN 512
-//#define CLOSESOCK(s) \
-//        if(INVALID_SOCKET != s) {closesocket(s); s = INVALID_SOCKET;}
-//#define ERR(e) \
-//        printf("%s:%s failed: %d [%s@%ld]\n",__FUNCTION__,e,WSAGetLastError(),__FILE__,__LINE__)
-//#define DEFAULT_WAIT    30000
-//#define WS_VER          0x0202
-//char recvbuf[DEFAULT_BUFLEN];
-//int iSendResult;
-//int recvbuflen = DEFAULT_BUFLEN;
-//HANDLE hCloseSignal = NULL;
-//
-//int main()
-//{
-	/*WSADATA wsadata;
-	int iResult = WSAStartup(MAKEWORD(2,2), &wsadata);
-	if (iResult)
-		printf("WSAStartup failed: %d\n", iResult);
-	struct addrinfo *pResult = NULL, *ptr = NULL, hints;
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET6;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &pResult);
-	if (iResult)
-	{
-		printf("getaddrinfo failed: %d\n", iResult);
-		WSACleanup();
-		return 1;
-	}
-
-	SOCKET ListenSocket = INVALID_SOCKET;
-	ListenSocket = socket(pResult->ai_family, pResult->ai_socktype, pResult->ai_protocol);
-	if (ListenSocket == INVALID_SOCKET)
-	{
-		printf("socket() failed: %d/n", WSAGetLastError());
-		freeaddrinfo(pResult);
-		WSACleanup();
-	}
-	iResult = bind(ListenSocket, pResult->ai_addr, (int)pResult->ai_addrlen);
-	if (iResult == SOCKET_ERROR)
-	{
-		printf("bind failed with error : %d\n", WSAGetLastError());
-		freeaddrinfo(pResult);
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-	else freeaddrinfo(pResult);
-	
-	if (listen(ListenSocket, SOMAXCONN)==SOCKET_ERROR)
-	{
-		printf("Listen failed with error: %ld\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-	SOCKET ClientSocket = INVALID_SOCKET;
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	if (ClientSocket == INVALID_SOCKET)
-	{
-		printf("accept failed: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	do
-	{
-		iResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN, 0);
-		if(iResult > 0)
-		{
-			printf("Bytes recieved: %d\n", iResult);
-			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-			if (iSendResult == SOCKET_ERROR)
-			{
-				printf("send failed: %d\n", WSAGetLastError());
-				closesocket(ClientSocket);
-				WSACleanup();
-				return 1;
-			}
-			printf("Bytes sent: %d\n", iSendResult);
-		}else if (iResult == 0) printf("Connection closing...\n");
-		else
-		{
-			printf("recv failed: %d\n", WSAGetLastError());
-			closesocket(ClientSocket);
-			WSACleanup();
-			return 1;
-		}
-	} while (iResult > 0);
-	iResult = shutdown(ClientSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR) {
-		printf("shutdown failed: %d\n", WSAGetLastError());
-		closesocket(ClientSocket);
-		WSACleanup();
-		return 1;
-	}
-	closesocket(ClientSocket);
-	WSACleanup();
-
-	return 0;*/
 #define PORT 5223
 #define DATA_BUFSIZE 8192
 
@@ -209,13 +106,13 @@ int main(int argc, char **argv)
 	else
 		printf("WSACreateEvent() is OK!\n");
 
-	if ((ThreadHandle = CreateThread(NULL, 0, WorkerThread, (LPVOID)AcceptEvent, 0, &ThreadId)) == NULL)
+	/*if ((ThreadHandle = CreateThread(NULL, 0, WorkerThread, (LPVOID)AcceptEvent, 0, &ThreadId)) == NULL)
 	{
 		printf("CreateThread() failed with error %d\n", GetLastError());
 		return 1;
 	}
 	else
-		printf("CreateThread() should be fine!\n");
+		printf("CreateThread() should be fine!\n");*/
 
 	while (TRUE)
 	{
@@ -228,8 +125,16 @@ int main(int argc, char **argv)
 			printf("WSASetEvent() failed with error %d\n", WSAGetLastError());
 			return 1;
 		}
-		else
-			printf("accept new client %s \n", inet_ntoa(ClientAddr.sin_addr));
+		else {
+			printf("accept new client %s\n", inet_ntoa(ClientAddr.sin_addr));
+			if ((ThreadHandle = CreateThread(NULL, 0, WorkerThread, (LPVOID)AcceptEvent, 0, &ThreadId)) == NULL)
+			{
+				printf("CreateThread() failed with error %d\n", GetLastError());
+				return 1;
+			}
+			else
+				printf("CreateThread() should be fine!\n");
+		}
 	}
 	return 0;
 }
@@ -294,7 +199,7 @@ DWORD WINAPI WorkerThread(LPVOID lpParameter)
 			printf("WSARecv() is OK!\n");
 
 		printf("Socket %d got connected...\n", AcceptSocket);
-		printf("Accept thread %d\n", (int)GetCurrentThreadId());
+		printf("Accept thread %d\n", (int)GetThreadId(GetCurrentThread()));
 	}
 	return TRUE;
 }
@@ -318,7 +223,6 @@ void CALLBACK WorkerRoutine(DWORD Error, DWORD BytesTransferred, LPWSAOVERLAPPED
 	DWORD Flags;
 
 	LPSOCKET_INFORMATION SI = (LPSOCKET_INFORMATION)Overlapped;
-	SOCKADDR_IN* client_addr = (SOCKADDR_IN*)Overlapped;
 	if (Error != 0)
 	{
 		printf("I/O operation failed with error %d\n", Error);
@@ -333,7 +237,7 @@ void CALLBACK WorkerRoutine(DWORD Error, DWORD BytesTransferred, LPWSAOVERLAPPED
 	{
 		closesocket(SI->Socket);
 		GlobalFree(SI);
-		printf("%d clinet %s disconected\n", (int)GetCurrentThreadId(), inet_ntoa(client_addr->sin_addr));
+		printf("%d clinet disconected\n", (int)GetCurrentThreadId());
 		return;
 	}
 
